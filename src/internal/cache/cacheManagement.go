794886ce457996ed30fe2090df567a4bf5c39a42
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-22 12:38:48 -0300

    Implementando Redis, TODO: Testes do Redis

---
+++ src/internal/cache/cacheManagement.go
@@ -0,0 +1,35 @@
+// Package cache gerencia o Redis
+package cache
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	RedisClient *redis.Client
+	Ctx         = context.Background()
+)
+
+func ConectarRedis() {
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	err := RedisClient.Ping(Ctx).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Não foi possível conectar ao Redis: %v", err))
+	}
+	fmt.Println("Redis conectado.")
+}
+
+func SalvarLinkRedis(codigo, url string) error {
+	return RedisClient.Set(Ctx, codigo, url, 0).Err()
+}
+
+func BuscarLinkRedis(codigo string) (string, error) {
+	return RedisClient.Get(Ctx, codigo).Result()
+}

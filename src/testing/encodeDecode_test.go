2aab7a4364939e93fc7c1b893a7ddb03decb3ada
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-21 23:23:25 -0300

    Finalizando testes de encoder e decoder. Funcionando perfeitamente. TODO: Fazer o Postgres e o Redis

---
+++ src/testing/encodeDecode_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/alvarolucio2007/encurtador-links-go/src/internal/shortener"
+)
+
+func TestEncodeDecode(t *testing.T) {
+	t.Run("Encode", func(t *testing.T) {
+		testarEncoding(t)
+	})
+	t.Run("Decode", func(t *testing.T) {
+		testarDecoding(t)
+	})
+}
+
+func testarEncoding(t *testing.T) {
+	testIDs := []struct {
+		nome    string
+		entrada uint64
+		saida   string
+	}{
+		{"ID Mil", 1000, "g8"},
+		{"ID Milhão", 10000000, "FXsk"},
+		{"ID Grande", 3810923821908, "155NdZ6A"},
+	}
+	for _, tc := range testIDs {
+		t.Run(tc.nome, func(t *testing.T) {
+			resultado := shortener.Encode(tc.entrada)
+			if resultado != tc.saida {
+				t.Errorf("Falha no teste [%s]: entrada %d gerou %s, mas queríamos %s",
+					tc.nome, tc.entrada, resultado, tc.saida)
+			}
+		})
+	}
+}
+
+func testarDecoding(t *testing.T) {
+	testIDs := []struct {
+		nome    string
+		entrada string
+		saida   uint64
+	}{
+		{"Nome pequeno", "g8", 1000},
+		{"Nome grande", "FXsk", 10000000},
+		{"Nome gigante", "155NdZ6A", 3810923821908},
+	}
+	for _, tc := range testIDs {
+		t.Run(tc.nome, func(t *testing.T) {
+			resultado := shortener.Decode(tc.entrada)
+			if resultado != tc.saida {
+				t.Errorf("Falha no teste [%s]: entrada %s gerou %v, mas queríamos %v",
+					tc.nome, tc.entrada, resultado, tc.saida)
+			}
+		})
+	}
+}
